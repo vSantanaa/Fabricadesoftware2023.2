e034f940191c56a9cad490c118427bb72025042b
vSantanaa <autologon@CSED.LAB>
2023-08-28 17:04:11 -0300

    Envio primeira atividade

---
+++ atividade01.py
@@ -0,0 +1,10 @@
+número = int(input("digite um número:"))
+
+sucessor = número + 1
+antecessor = número - 1
+#A variável int também poderia ser atribuíada aqui 
+
+print(f"o sucessor desse número é: {sucessor}")
+print(f"o antecessor desse número é: {antecessor}")
+
+#f = uma das variantes de formatação
